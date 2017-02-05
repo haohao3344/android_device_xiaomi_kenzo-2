5a977fda34b2860b511c66f1c20645e81cd624db
gismoZ <tolis_kon@yahoo.gr>
2017-02-05 18:16:41 +0200

    msm8956: Drop XML audio policy

--- device.mk
+++ device.mk
@@ -91,10 +91,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_wcd9326.xml
 
-#XML Audio configuration files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-
 # Browser
 PRODUCT_PACKAGES += \
     Gello
